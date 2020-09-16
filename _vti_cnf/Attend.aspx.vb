94b31c13c887eb809c782cc70279ccd6329ee1a2
JeffMorton <jbm@virginia.edu>
2020-09-16 11:38:53 -0400

    Update Attend.aspx.vb

--- _vti_cnf/Attend.aspx.vb
+++ _vti_cnf/Attend.aspx.vb
@@ -1,5 +1,5 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|05 Aug 2020 14:24:01 -0000
+vti_timelastmodified:TR|26 Aug 2020 18:16:46 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_cacheddtm:TX|29 May 2020 16:14:39 -0000
 vti_filesize:IR|2465
