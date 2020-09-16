13138f9c2450be8954e4cb758ca909225d54b83e
JeffMorton <jbm@virginia.edu>
2020-09-16 11:39:29 -0400

    Update PayPalInfo.aspx.designer.vb

--- _vti_cnf/PayPalInfo.aspx.designer.vb
+++ _vti_cnf/PayPalInfo.aspx.designer.vb
@@ -1,8 +1,9 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|19 Nov 2019 14:03:10 -0000
+vti_timelastmodified:TR|24 Aug 2020 19:19:31 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_cacheddtm:TX|19 Nov 2019 14:03:10 -0000
 vti_filesize:IR|2768
 vti_backlinkinfo:VX|
 vti_syncofs_ccfrcville.org\:21:TW|18 Jul 2020 14:16:00 -0000
 vti_syncwith_ccfrcville.org\:21:TX|19 Nov 2019 14:03:10 -0000
+vti_modifiedby:SR|JEFF-PC\\jb_mo
